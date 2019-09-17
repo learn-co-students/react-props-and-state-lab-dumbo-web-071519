798d30a521afc85e841b90df664aa9724d66a8d5
jason <jasonedlewis@gmail.com>
2019-09-17 16:48:22 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
+        type: "all"
       }
+    };
+  }
+
+  const onChangeType =()=>{
+    this.setState({
+        filters.type: 
+        }
+    })
+      
     }
+    
+
+  }
+  const onFindPetsClick=()=>{
+
   }
 
   render() {
     return (
-      <div className="ui container">
+      <div className="ui container" style={{ width: "80%", margin: "5% auto" }}>
         <header>
           <h1 className="ui dividing header">React Animal Shelter</h1>
         </header>
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
             </div>
             <div className="twelve wide column">
               <PetBrowser />
@@ -32,8 +46,8 @@ class App extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
