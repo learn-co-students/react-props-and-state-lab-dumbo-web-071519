3147cb3947aaefc567c18d11a1ebda5dee478f84
Alessandra Vertrees <avertrees447@gmail.com>
2019-09-18 08:55:22 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,32 @@ import React from 'react'
 
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
+// ### `App`
+
+// 1.  The app's initial state is already defined. App has two children: the
+//   `<Filters />` and`<PetBrowser />` components.
+
+// - App should pass a ** callback ** prop, `onChangeType`, to`<Filters />`.This
+// callback needs to update`<App />`'s `state.filters.type`
+
+//   - `<Filters />` needs a ** callback ** prop, `onFindPetsClick`.When the
+//     `<Filters />` component calls`onFindPetsClick`, `<App />` should fetch a list
+// of pets using`fetch()`.
+
+//   - The URL of the API is`/api/pets` with an ** optional query parameter **.
+//   - Use app's state.filters to control/update this parameter
+//   - If the`type` is`'all'`, send a request to`/api/pets`
+//     - If the`type` is`'cat'`, send a request to`/api/pets?type=cat`.Do the
+// same thing for `dog` and`micropig`.
+//   - Finally set`<App/>`'s `state.pets` with the results of your fetch request
+// so you can pass the pet data down as props to`<PetBrowser />`
+
+//   * ** Even though we're using `fetch` here, its responses have been mocked in
+// order to make the tests work properly.That means it's important to use the
+// _exact_ URLs as described above, or your tests will fail! **
+
+//   The pet data received should include information on individual pets and their
+// adoption status.
 
 class App extends React.Component {
   constructor() {
@@ -14,6 +40,59 @@ class App extends React.Component {
       }
     }
   }
+  
+  onAdoptPet = (id) => {
+    //console.log(event)
+    console.log(id)
+    let pets = this.state.pets
+    pets.map( (pet) => {
+      if (pet.id === id){
+        pet.isAdopted = true
+      }
+    })
+    this.setState({
+      pets
+    })
+  }
+
+  onChangeType = (event) => {
+    //console.log(event.target.value)
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    },()=>console.log(this.state))
+  }
+
+  onFindPetsClick= (event) => {
+    //   - Use app's state.filters to control/update this parameter
+//   - If the`type` is`'all'`, send a request to`/api/pets`
+//     - If the`type` is`'cat'`, send a request to`/api/pets?type=cat`.Do the
+// same thing for `dog` and`micropig`.
+//   - Finally set`<App/>`'s `state.pets` with the results of your fetch request
+// so you can pass the pet data down as props to`<PetBrowser />`
+    let url = '/api/pets'
+    if (this.state.filters.type == 'cat'){
+      url = '/api/pets?type=cat'
+      //console.log("cat")
+    } else if (this.state.filters.type=='dog'){
+      url = '/api/pets?type=dog'
+      //console.log("dog")
+    } else if (this.state.filters.type == "micropig"){
+      url ='/api/pets?type=micropig'
+      //console.log("micropig")
+    } else {
+      url = '/api/pets'
+    }
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(data =>
+      this.setState({
+        pets: data
+      }, () => console.log(this.state))
+    )
+  }
 
   render() {
     return (
@@ -24,10 +103,13 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+                onFindPetsClick={this.onFindPetsClick}
+                onChangeType={this.onChangeType}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets}/>
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,48 @@
 import React from 'react'
+// ### `Pet`
+
+// 1.  Should receive a`pet` prop.Use the attributes in this data to render the
+// pet card correctly.It should show the pet's `name`, `type`, `age` and `weight`.
+// Based on the pet's `gender`, the component also needs to contain either a
+// male(`♂`) or female(`♀`) symbol.
+
+// - Should receive an`isAdopted` prop.Using this prop, render the correct button
+//   in the pet's card; if the pet is adopted, show the disabled button. Otherwise,
+// show the primary button to adopt the pet.
+
+// - Should receive an`onAdoptPet` callback prop.This callback prop gets called
+// with the pet's `id` when the user clicks the adopt pet button — _not_ when they
+// click the disabled button!
 
 class Pet extends React.Component {
+  // constructor(props){
+  //   console.log(this.props)
+  //   super(props)
+  //   this.state={
+
+  //   }
+  // }
   render() {
+    //console.log(this.props)
     return (
       <div className="card">
         <div className="content">
           <a className="header">
+            {this.props.pet.name} 
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.gender === 'male' ? `♂` : `♀`}
+          
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age:{this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.props.pet.isAdopted ? <button className="ui disabled button">Already adopted</button> : <button onClick={() => this.props.onAdoptPet(this.props.pet.id)} className="ui primary button">Adopt pet</button> }
         </div>
       </div>
     )
